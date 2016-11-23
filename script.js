8281fe821d83ee60fc8b0ce2370347c5e17e8381
Victor <unicaxverdad@gmail.com>
2016-11-23 06:33:18 +0300

    food/ coordinates/ fixes/ features
    food appearance/ disappearance when I move there;
    now program generates random x/y coordinates and calculate cell's
    position using this values;
    "game over" logic;
    other fixes are done.

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-//Created by Victor Kapustin on 3 may 2016.
-// IDE WebStorm. OS Ubuntu 14.04
-
-//Class
 // Create matrix with settings number of cols and rows
 
 //Functions:
@@ -18,38 +14,75 @@ function Matrix(cols, rows) {
     this.cols = cols;
     this.rows = rows;
     this.cellsAmount = this.cols * this.rows;
-    var matrix = this.matrix;
     this.matrix = document.getElementById("matrix");
 
     for (var i = 0; i < this.cellsAmount; i++) {
-
         var div = document.createElement("div");
         div.className = "cell " + i;
         this.matrix.appendChild(div);
     }
-    // console.log(Matrix);
-
-    this.gameOver = function() {
-        alert("GAME OVER");
-    };
 }
+
+function BodyPart() {
+    this.positionX = 0;
+    this.positionY = 0;
+    this.direction = "up";
+}
+
+function Food() {
+    this.position = 0;
+}
+
+Food.prototype.getPosition = function() {
+    return this.position;
+};
+
+Food.prototype.remove = function() {
+    this.position = -1;
+};
+
+/***
+ * Show image, when game is over
+ */
+Matrix.prototype.gameOver = function() {
+    alert("GAME OVER");
+    document.body.removeChild(document.body.children[0]);
+    var snakeWrapper = document.createElement("div");
+    var snakeImage = document.createElement("img");
+    snakeWrapper.className = "snake-wrapper";
+    snakeImage.className = "snake-image";
+    snakeImage.setAttribute("src", "img/dead-snake.jpg")
+    document.body.appendChild(snakeWrapper);
+    document.body.children[0].appendChild(snakeImage);
+};
 
 /***
  * Add color to random cell
  */
-Matrix.prototype.generateSnakeCell = function() {
-    var diapason = this.cellsAmount;
-    var randomDivNumber = Math.floor(Math.random() * diapason + 1);
-    return randomDivNumber;
+Matrix.prototype.generateRandomCell = function() {
+    var colsDiapason = this.cols;
+    var rowsDiapason = this.rows;
+    var randomXCoordinate = Math.floor(Math.random() * colsDiapason + 1);
+    var randomYCoordinate = Math.floor(Math.random() * rowsDiapason + 1);
+    return this.calculateCellPosition(randomXCoordinate, randomYCoordinate);
+};
+
+/***
+ * Calculating position of cell using it's coordinates
+ */
+Matrix.prototype.calculateCellPosition = function(xCoordinate, yCoordinate) {
+    return (xCoordinate - 1) * this.cols + yCoordinate;
 };
 
 /***
  * Add color to cell
  * @param cellNumber
  */
-Matrix.prototype.colouredCell = function(cellNumber) {
-    if(this.matrix.children[cellNumber].style.backgroundColor != "black"){
-        this.matrix.children[cellNumber].style.backgroundColor = "black";
+Matrix.prototype.colouredCell = function(cellNumber, colour) {
+    if (colour == undefined)
+        colour = "green";
+    if(this.matrix.children[cellNumber].style.backgroundColor != colour){
+        this.matrix.children[cellNumber].style.backgroundColor = colour;
     }
 };
 
@@ -70,19 +103,45 @@ Matrix.prototype.uncolouredCell = function(cellNumber) {
 Matrix.prototype.changePosition = function(currentCell, directionNumber) {
     this.uncolouredCell(currentCell);
     currentCell += directionNumber;
-    this.colouredCell(currentCell);
+    this.colouredCell(currentCell, "black");
     return currentCell;
 };
 
 /***
+ * Calculating cell's coordinates, using position number and x/y values of matrix
+ */
+//TODO fix algorythm
+Matrix.prototype.getCoordinatesFromPosition = function(position) {
+    var xCoordinate = 0;
+    var yCoordinate = 0;
+
+    if((position % this.cols) === 0) {
+        xCoordinate = this.cols;
+        yCoordinate = position / this.cols;
+    } else if ((position % this.cols) > 0) {
+
+        if (position < this.cols) {
+            xCoordinate = position % this.cols;
+            yCoordinate = 0;
+        } else {
+            xCoordinate = position % this.cols;
+            yCoordinate = Math.ceil(position / this.cols);
+        }
+    }
+    yCoordinate = position % this.cols;
+    var coordinates = [xCoordinate, yCoordinate];
+    return coordinates;
+};
+
+/***
  * Remove color from old position, change cell number according to key number and add color to the new
+ * 37 - Left, 38 - Up, 39 - Right, 40 - Down
  * @param int movingCell
  * @returns int movingCell
  */
 Matrix.prototype.keyPressHandle = function(direction) {
     switch(direction) {
         case 37:
-		
             if ((Matrix.currentCell == 0) || (Matrix.currentCell % this.cols == 0)) {
                 this.gameOver();
                 break;
@@ -95,7 +154,6 @@ Matrix.prototype.keyPressHandle = function(direction) {
                 break;
             }
             Matrix.currentCell = this.changePosition(Matrix.currentCell, -20);
-            console.log(Matrix.currentCell);
             break;
         case 39:
             if ((Matrix.currentCell + 1 > this.cellsAmount) || (Matrix.currentCell + 1) % 20 == 0) {
@@ -105,7 +163,7 @@ Matrix.prototype.keyPressHandle = function(direction) {
             Matrix.currentCell = this.changePosition(Matrix.currentCell, 1);
 			break;
         case 40:
-            if (Matrix.currentCell + 20 > this.cellsAmount) {
+            if (Matrix.currentCell + 20 >= this.cellsAmount) {
                 this.gameOver();
                 break;
             }
@@ -116,14 +174,46 @@ Matrix.prototype.keyPressHandle = function(direction) {
 
 window.onload = function() {
     var myMatrix = new Matrix(20, 20);
-    Matrix.currentCell = 190;
-    myMatrix.colouredCell(Matrix.currentCell);
 
+    Matrix.currentCell = myMatrix.generateRandomCell();
+
+    // var coordinates = myMatrix.getCoordinatesFromPosition(currentCell);
+
+    // console.log(coordinates);
+    myMatrix.colouredCell(Matrix.currentCell, "black");
+    var food = new Food();
+
+    setTimeout(function(){
+        food.position = myMatrix.generateRandomCell()
+        myMatrix.colouredCell(food.getPosition());
+    },1000);
+
+    //Snake
+    snake = new Array();
+
+    var head = new BodyPart();
+
+    
+
+    console.log(Matrix.currentCell);
+    //Button click handler
     document.onkeydown = function(event) {
-
         if((event.keyCode >= 37) && (event.keyCode <= 40)) {
             var keyCode = event.keyCode;
             myMatrix.keyPressHandle(keyCode);
+
+            if(Matrix.currentCell == food.getPosition()){
+                food.remove();
+
+
+
+                setTimeout(function(){
+                    food.position = myMatrix.generateRandomCell();
+                    myMatrix.colouredCell(food.getPosition());
+                }, 2000
+                );
+
+            }
         }
     }
 
